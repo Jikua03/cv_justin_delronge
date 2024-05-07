ed1b87c69fc0ceaecfcc8458e6e475af3eff63d2
Jikua03 <112984477+Jikua03@users.noreply.github.com>
2024-05-07 17:31:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-if (navigator.userAgent.match(/samsung/i)) {
-       alert("Your browser (Samsung Internet) may not show this website" + 
-             "correctly. Please consider using a standards-compliant" + 
-             "browser instead. \n\n" +
-             "We recommend Firefox, Microsoft Edge, or Google Chrome.");
-   }
 hamburger = document.querySelector(".hamburger");
 hamburger.onclick = function() {
 	navBar = document.querySelector(".nav-bar");
